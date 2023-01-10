27d2a9905e47922edd088108909cf11fb09acf30
hftamayo <hftamayo@gmail.com>
2023-01-09 20:11:22 -0600

    first version ready for deployment

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xcDA084dF8CA647603E7b32d0b82F499657Ff66E8";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
